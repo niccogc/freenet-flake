26295a93b24c91c7031e992580615bcc9ef2faa2
niccogc <92951857+niccogc@users.noreply.github.com>
2026-02-21 23:33:02 +0000

    chore: update freenet to 0.1.142

--- nix/packages/freenet.nix
+++ nix/packages/freenet.nix
@@ -6,10 +6,10 @@
 }:
 stdenv.mkDerivation rec {
   pname = "freenet-core";
-  version = "0.1.139"; # freenet-version
+  version = "0.1.142"; # freenet-version
   src = fetchurl {
     url = "https://github.com/freenet/freenet-core/releases/download/v${version}/freenet-x86_64-unknown-linux-musl.tar.gz";
-    hash = "sha256-OEhkP9okNt+3XmOTQ8dLoeD/4KwJFHyOBLNlmsipdjg=";
+    hash = "sha256-lAYu7cPMfX8fl+wRhPUtoPA+1rEYbcdVhGGxXsn0tus=";
   };
 
   nativeBuildInputs = [autoPatchelfHook];
